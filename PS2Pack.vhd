b1edb5ecec68e629225cd108e15674a5647f4a18
Erik Zachrisson <erik@zachrisson.info>
2014-10-11 23:04:08 +0200

    Add more reg constants

--- PS2Pack.vhd
+++ PS2Pack.vhd
@@ -9,9 +9,14 @@ package PS2Pack is
   constant PS2Addr       : integer := 16#00000000#;
   constant PS2State      : integer := 16#00000001#;
   constant PS2Sampler    : integer := 16#00000002#;
+  constant PS2InitState  : integer := 16#00000003#;
+  --
+  constant MousePacketState  : integer := 16#00000004#;  
   --
   constant SerFifoLvl    : integer := 16#00000010#;
   constant SerFifoMaxLvl : integer := 16#00000011#;
+  --
+
 end package;
 
 package body PS2Pack is
